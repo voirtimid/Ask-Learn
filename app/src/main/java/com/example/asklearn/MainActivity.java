fafc1fcdd84f0d20c12e4a4a854ede98da3aefbc
anton31kah <anton.kahwaji@gmail.com>
2018-11-30 00:09:09 +0100

    Anton's first commit

--- app/src/main/java/com/example/asklearn/MainActivity.java
+++ app/src/main/java/com/example/asklearn/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.asklearn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void clickMeOnClick(View view) {
+        Toast.makeText(this, "Hello from the other side", Toast.LENGTH_LONG).show();
     }
 }
